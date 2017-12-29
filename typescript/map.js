35d180e62247115eb10bfd7c61737ab6e0152954
Adam <adam@planswift.com>
2017-12-28 17:50:30 -0700

    comments and cleanup

--- typescript/map.js
+++ typescript/map.js
@@ -1,4 +1,5 @@
 var map = /** @class */ (function () {
+    //generates map pieces and populates them with info
     function map(height, width) {
         this.height = height;
         this.width = width;
@@ -6,12 +7,13 @@ var map = /** @class */ (function () {
         for (var i = 0; i < this.width; i++) {
             grid[i] = new Array(this.height);
             for (var j = 0; j < this.height; j++) {
+                //base case check for two populated neighbors and get close to those
                 if (i != 0 && j != 0) {
                     var max = grid[i - 1][j];
                     var min = grid[i][j - 1];
                     if (min > max) {
-                        var min_1 = grid[i - 1][j];
-                        var max_1 = grid[i][j - 1];
+                        min = grid[i - 1][j];
+                        max = grid[i][j - 1];
                     }
                     grid[i][j] = gaussianRandom(min - 1, max + 1);
                 }
@@ -29,6 +31,7 @@ var map = /** @class */ (function () {
         this.grid = grid;
         // console.log(grid);
     }
+    //displays the map using color map and elevation values
     map.prototype.display = function () {
         var boxSize = 1;
         var output = "<table style='border-collapse: collapse;'>";
@@ -47,9 +50,11 @@ var map = /** @class */ (function () {
     };
     return map;
 }());
+//TODO: move this to a utility area when we get there
 function gaussianRandom(start, end) {
     return Math.abs(Math.floor(start + gaussianRand() * (end - start + 1)));
 }
+//TODO: move this to a utility area when we get there
 function gaussianRand() {
     var rand = 0;
     for (var i = 0; i < 6; i += 1) {
@@ -57,26 +62,28 @@ function gaussianRand() {
     }
     return rand / 6;
 }
-var mapColors = ['000066',
-    '0000cc',
-    '0000cc',
-    '0000ff',
-    'ffff66',
-    '00ff00',
-    '00cc00',
-    '009933',
-    '666633',
-    '333300',
-    'e0e0d1',
-    'ebebe0',
-    'f5f5f0',
-    'ffffff',
-    'ffffff',
-    'ffffff',
-    'ffffff',
-    'ffffff',
-    'ffffff',
-    'ffffff',
-    'ffffff']; //more snow
+//hex values to be used for the map display
+var mapColors = ['#000066',
+    '#0000cc',
+    '#0000cc',
+    '#0000ff',
+    '#ffff66',
+    '#00ff00',
+    '#00cc00',
+    '#009933',
+    '#666633',
+    '#333300',
+    '#e0e0d1',
+    '#ebebe0',
+    '#f5f5f0',
+    '#ffffff',
+    '#ffffff',
+    '#ffffff',
+    '#ffffff',
+    '#ffffff',
+    '#ffffff',
+    '#ffffff',
+    '#ffffff']; //more snow
+//run the prototype
 var gameMap = new map(250, 250);
 document.body.innerHTML = gameMap.display();
--- typescript/map.ts
+++ typescript/map.ts
@@ -1,31 +1,33 @@
 class map {
-
+    //2 dimensional array for map pieces - currently just elevation numbers
     private grid: number[][];
+    //generates map pieces and populates them with info
     constructor(public height: number, public width: number) {
         let grid = new Array(this.height);
         for (let i:number  = 0; i < this.width; i++) {
             grid[i] = new Array(this.height);
 
             for (let j:number = 0; j < this.height; j++) {
+                //base case check for two populated neighbors and get close to those
                 if(i != 0 && j != 0) {
                     let max = grid[i-1][j];
                     let min = grid[i][j-1];
                     if(min > max) {
-                        let min = grid[i-1][j];
-                        let max = grid[i][j-1];
+                        min = grid[i-1][j];
+                        max = grid[i][j-1];
                     }
-                    //alternate rows so that there are not diagonal stripes
-                    if(i%2==1) {
-                        grid[this.width - i][j] = gaussianRandom(min - 1, max + 1);
-                    } else {
-                        grid[i][j] = gaussianRandom(min - 1, max + 1);
-                    }
-                } else if (i!=0) {
+                    grid[i][j] = gaussianRandom(min - 1, max + 1);
+                }
+                //first row, get close to left neighbor
+                else if (i!=0) {
                     grid[i][j] = gaussianRandom(grid[i-1][j]-1, grid[i-1][j]+1);
-                } else if (j!=0) {
+                }
+                //first column, get close to upstairs neighbor
+                else if (j!=0) {
                     grid[i][j] = gaussianRandom(grid[i][j-1]-1, grid[i][j-1]+1);
-                } else {
-
+                }
+                //first map piece, pick something random
+                else {
                     grid[i][j] = gaussianRandom(0, 10);
                 }
             }
@@ -35,6 +37,7 @@ class map {
         // console.log(grid);
     }
 
+    //displays the map using color map and elevation values
     display() {
         let boxSize:number = 1;
         let output = "<table style='border-collapse: collapse;'>";
@@ -53,9 +56,11 @@ class map {
     }
 }
 
+//TODO: move this to a utility area when we get there
 function gaussianRandom(start, end) {
     return Math.abs(Math.floor(start + gaussianRand() * (end - start + 1)));
 }
+//TODO: move this to a utility area when we get there
 function gaussianRand() {
     let rand = 0;
 
@@ -66,29 +71,30 @@ function gaussianRand() {
     return rand / 6;
 }
 
-let mapColors = ['000066',//deep water
-                '0000cc',//deep water
-                '0000cc',//deep water
-                '0000ff',//water
-                'ffff66',//sand
-                '00ff00',//grass
-                '00cc00',//brush
-                '009933',//forest
-                '666633',//rock
-                '333300',//stone
-                'e0e0d1',//snow
-                'ebebe0',//snow
-                'f5f5f0',//snow
-                'ffffff',//snow
-                'ffffff',//snow
-                'ffffff',//snow
-                'ffffff',//snow
-                'ffffff',//snow
-                'ffffff',//snow
-                'ffffff',//snow
-                'ffffff'];//more snow
+//hex values to be used for the map display
+let mapColors = ['#000066',//deep water
+                '#0000cc',//deep water
+                '#0000cc',//deep water
+                '#0000ff',//water
+                '#ffff66',//sand
+                '#00ff00',//grass
+                '#00cc00',//brush
+                '#009933',//forest
+                '#666633',//rock
+                '#333300',//stone
+                '#e0e0d1',//snow
+                '#ebebe0',//snow
+                '#f5f5f0',//snow
+                '#ffffff',//snow
+                '#ffffff',//snow
+                '#ffffff',//snow
+                '#ffffff',//snow
+                '#ffffff',//snow
+                '#ffffff',//snow
+                '#ffffff',//snow
+                '#ffffff'];//more snow
 
 
-
+//run the prototype
 let gameMap = new map(250,250);
 document.body.innerHTML = gameMap.display();
